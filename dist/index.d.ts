fe8e92202e654182648d67a03c72581b24e54244
Pierre-Olivier Mauguet <mauguet.po@gmail.com>
2025-02-20 13:43:47 +0100

    feat: remove dist and configure tsconfig

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ClipOptions {
-    tags?: string[];
-    comment?: string;
-    outputDir?: string;
-    url: string;
-}
-export declare function clipLink({ url, tags, comment, outputDir }: ClipOptions): Promise<string>;
